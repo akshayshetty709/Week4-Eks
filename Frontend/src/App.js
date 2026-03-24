cdd11a4c9c59a2796add83d977978dc7ae5dae0b
akshayshetty709 <114096841+akshayshetty709@users.noreply.github.com>
2026-03-24 10:30:18 +0530

    Create App.js

---
+++ Frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("/api")
+      .then(res => res.text())
+      .then(data => setMessage(data))
+      .catch(() => setMessage("Error connecting backend"));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 3-Tier DevOps Project</h1>
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default App;
