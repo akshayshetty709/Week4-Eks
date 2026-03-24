25ec9fca2ce72e221022b39ad90b5b42c5aae767
akshayshetty709 <114096841+akshayshetty709@users.noreply.github.com>
2026-03-24 10:32:50 +0530

    Create app.js

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.listen(3000, () => console.log("Server running"));
